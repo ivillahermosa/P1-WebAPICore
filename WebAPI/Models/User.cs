9522afdaa2c3a0dee09e7414a2d469ac3893ff31
Ivan <villahermosaivan82@gmail.com>
2021-04-07 20:31:23 +0800

    first commit

---
+++ WebAPI/Models/User.cs
@@ -0,0 +1,12 @@
+namespace WebAPI.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public String Firstname { get; set; }
+        public String Middlename { get; set; }
+        public String Lastname { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+    }
+}
